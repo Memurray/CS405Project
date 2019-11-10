2f510963761e3ebe65fa193db5698320fc5f0ea6
Memurray <memu225@uky.edu>
2019-11-09 20:47:11 -0500

    Add sales stats, order filtering

---
+++ buildOrderDisplay.php
@@ -0,0 +1,67 @@
+<?php
+include('dbConnect.php');
+
+echo '<table border="1"><tr>';
+echo '<td width="130px"><b>Order Number</b></td>';
+echo '<td width="160px"><b>User name</b></td>';
+echo '<td width="160px"><b>Status</b></td>';
+echo '<td width="170px"><b>Time</b></td>';
+echo '<td width="200px"><b>Order Contents</b></td>';
+echo '<td width="160px"><b>Price ($)</b></td></tr>';
+
+$filter = $_POST["category"];
+
+if($filter == "All")
+	$query = "SELECT * FROM orders WHERE status != 'Cart'";
+else
+	$query = "SELECT * FROM orders WHERE status = 'Pending' ";
+$statement = $connect->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+$total_row = $statement->rowCount();
+
+$i=1;
+foreach($result as $row) {
+    echo '<tr>';
+    $id = $row['id'];
+    $name = $row['user_name'];
+    $status = $row['status'];
+    $time = $row['placed_at'];
+    $price = $row['price'];
+
+    $query = "SELECT * FROM order_items, products WHERE order_id = " . $id . " AND product_name = name";
+    $statement = $connect->prepare($query);
+    $statement->execute();
+    $result2 = $statement->fetchAll();
+
+    $valid = 1;
+    foreach($result2 as $row2){
+   	if( $row2['stock_remaining'] < $row2['quantity'])
+            $valid = 0;
+    }
+
+    echo '<td id= n' . $i .'>' . $id . '</td>';
+    echo '<td>' . $name . '</td>';
+    if($status == "Pending" and $valid)
+	echo '<td>' . $status . ' <button class="statusEdit" val = ' . $i . '>Ship It</button></td>';
+    else if($status == "Pending" and !$valid)
+        echo '<td class="error">' . $status . ' (Err:Inventory)</td>';
+    else
+	echo '<td>' . $status . '</td>';
+    echo '<td>' . $time . '</td>';
+    echo '<td>';
+    $valid = 1;
+    foreach($result2 as $row2){
+	if( $row2['stock_remaining'] < $row2['quantity'] and $status == "Pending")
+	    echo "<div class='error'> " . $row2['product_name'] . " x" . $row2['quantity'] ." (Left: " . $row2['stock_remaining'] . ")</div>";
+	else
+	    echo "<div> " . $row2['product_name'] . " x" . $row2['quantity'] ."</div>";
+    }
+    echo '</td>';
+    echo '<td>' . $price . '</td>';
+    echo '</tr>';
+    $i = $i+1;
+}
+echo '</table>';
+
+?>
--- stats.php
+++ stats.php
@@ -10,18 +10,20 @@ float: right;
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<title>Customer Orders</title>
+<title>Sales Statistics</title>
 
 <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
 <div class="main">
-<h1>Customer Orders</h1>
+<h1>Product Sale Statistics</h1>
 
-<b>Status Filter:</b>
-  <input type="radio" name="cat" class="select category" id="radioAllCategories" value="All"  checked > All 
-  <input type="radio" name="cat" class="select category" id="radioPending" value="Pending" > Pending<br><br>
+<b>Historical Sales:</b>
+  <input type="radio" name="cat" class="select category" id="radioAllCategories" value="All"  checked > All time 
+  <input type="radio" name="cat" class="select category" id="radio7day" value=7> Last 7 Days
+  <input type="radio" name="cat" class="select category" id="radio1month" value=30 > Last Month
+  <input type="radio" name="cat" class="select category" id="radio1year" value=365 > Last Year<br><br>
 
 <div class="filter">
 </div>
@@ -33,14 +35,14 @@ $(document).ready(function(){
     filter();
     function filter(){
 	$.ajax({
-            url:"buildOrderDisplay.php",
+            url:"buildStats.php",
             method:"POST",
             data:{category:category},
             success:function(data){
 	     $('.filter').html(data);
             }
         });
-	}
+    }
     $('.select').click(function(){
 	var changed = false;
 	var tempCat = $("input[name=cat]:checked").val()
@@ -52,22 +54,8 @@ $(document).ready(function(){
 		filter();
 	}
     });
-
-    $('body').on('click', '.statusEdit', function (){
-	var clickRow = $(this).attr('val');
-	var idLookup = "n" + clickRow;
-	var id = document.getElementById(idLookup).innerHTML;
-	$.ajax({
-            url:"shipOrder.php",
-            method:"POST",
-            data:{id:id},
-            complete: function(data){
-		filter();
-            }
-        });
-    });
-
 });
+
 </script>
 
 </div>
